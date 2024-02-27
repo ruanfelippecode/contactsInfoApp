3437f12a6a5a703d64825712577b9b22d5e2f67e
Ruan Felippe <122935680+ruanfelippecode@users.noreply.github.com>
2024-02-27 16:08:12 -0300

    header.js

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -5,7 +5,7 @@ const Header = () => {
     <div className="perfil_conteudo">
       <div className="perfil_imagem"></div>
       <h3>Ruan Felippe</h3>
-      <p>Desenvolvedor Back-end JavaScript</p>
+      <p>Desenvolvedor Back-End</p>
     </div>
   );
 };
